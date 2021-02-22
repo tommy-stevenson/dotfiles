ae0ab5112d8b4b281417bc2d985e0d90f9886230
Tommy Stevenson <tommy@stevenson.us>
2021-02-22 10:21:49 -0600

    update: todo for brave check

--- depend.sh
+++ depend.sh
@@ -20,12 +20,14 @@ sudo apt update
 
 sudo apt install $DEPEND -y
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 sudo apt update
 
-sudo apt install brave-browser
+#sudo apt install brave-browser
 
+#TODO: check to see if brave key is in keyring and if repo is in sources.list
 
